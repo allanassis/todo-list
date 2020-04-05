d8284f62b8f8bb5e9fd308a926db1afc36d9e61b
Allan Assis <allan.andrade@corp.globo.com>
2020-04-05 19:58:53 -0300

    Add post route to create a task

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"net/http"
-
+	"github.com/allanassis/todo-list/models"
 	"github.com/labstack/echo/v4"
+	"net/http"
+	"time"
 )
 
 func main() {
@@ -11,5 +12,17 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	e.POST("/task", func(c echo.Context) error {
+		t := &models.Task{
+			CreatedDate: time.Now(),
+		}
+		if err := c.Bind(t); err != nil {
+			return err
+		}
+		t.Save()
+		return c.JSON(http.StatusCreated, t)
+	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
